b56306f2d5979b0c3a4eae03b3550dd9bdd744b1
Tommy Quatretemps <Tommyq70@gmail.com>
2026-02-06 21:13:08 +0100

    first commit

---
+++ ytslop.js
@@ -0,0 +1,8 @@
+const style = document.createElement("style");
+style.textContent = `
+ytd-rich-section-renderer, yt-horizontal-list-renderer {
+  display: none !important;
+}`;
+document.head.appendChild(style);
+
+  
